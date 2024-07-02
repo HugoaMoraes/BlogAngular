24d2695bec721925067b9bb39dad47568cde4ef9
Hugo Moraes <hugomoraes2007@gmail.com>
2024-07-02 15:25:28 -0300

    primeiro commit

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,36 @@
 export const dataFake = [
   {
-    "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    id: '1',
+    title: 'Bem-vindo ao tutorial Angular',
+    description:
+      'Você precisará ter familiaridade básica com HTML, CSS e JavaScript para entender o Angular. Cada passo representa um conceito em Angular. Você pode fazer um ou todos eles. Se você ficar preso, clique em Revelar resposta no topo. Muito bem, vamos.',
+    photoCover: 'https://angular.dev/assets/images/ng-image.jpg',
+    link: 'https://angular.dev/tutorials/learn-angular',
   },
   {
-    "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
-  }
-]
+    id: '2',
+    title: 'Componentes em Angular',
+    description:
+      'Os componentes são os blocos de construção fundamentais para qualquer aplicação Angular. Cada componente tem três partes: *Classe de TypeScript *Modelo de HTML *Estilos de CSS. Nesta atividade, você aprenderá como atualizar o modelo e os estilos de um componente.',
+    photoCover:
+      'https://img-b.udemycdn.com/course/750x422/4471116_dcf0_2.jpg',
+    link: 'https://angular.dev/tutorials/learn-angular/1-components-in-angular',
+  },
+  {
+    id: '3',
+    title: 'Comunicação de Componentes com @Input',
+    description:
+      'Às vezes, o desenvolvimento de aplicativos exige o envio de dados para um componente. Esses dados podem ser usados ​​para personalizar um componente ou talvez enviar informações de um componente pai para um componente filho. Angular usa um conceito chamado Input. Isso é semelhante aos adereços em outras estruturas. Para criar uma propriedade Input, use o decorador @Input. Nesta atividade, você aprenderá como usar o decorador @Input para enviar informações aos componentes.',
+    photoCover:
+      'https://ionic.io/blog/wp-content/uploads/2024/02/angular-feature-image-1.png',
+    link: 'https://angular.dev/tutorials/learn-angular/8-input',
+  },
+  {
+    id: '4',
+    title: 'Defina uma rota',
+    description: 'Agora que configurou o aplicativo para usar o Angular Router, você precisa definir as rotas. Nesta atividade, você aprenderá como adicionar e configurar rotas com seu aplicativo.',
+    photoCover:
+      'https://ionic.io/blog/wp-content/uploads/2023/12/angular-feature-image.png',
+    link: 'https://angular.dev/tutorials/learn-angular/13-define-a-route',
+  },
+];
